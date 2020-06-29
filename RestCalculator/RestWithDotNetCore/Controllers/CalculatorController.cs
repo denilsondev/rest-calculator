58157fbf12d5c0614ad086ef96569c3e07ca007d
denilsondev <denilsoncdc10@gmail.com>
2020-06-29 19:04:16 -0300

    calculator developed with dotnet core

---
+++ RestCalculator/RestWithDotNetCore/Controllers/CalculatorController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RestWithDotNetCore.Utils;
+
+namespace RestWithDotNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+    
+        private readonly Utils.Utils _utils; 
+        public CalculatorController()
+        {
+             _utils = new Utils.Utils();
+        }
+
+        // GET api/values/5
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Sum(string firstNumber, string secondNumber)
+        {
+            if (_utils.IsNumeric(firstNumber) && _utils.IsNumeric(secondNumber))
+            {
+                var sum = _utils.ConvertToDecimal(firstNumber) + _utils.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/5
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Subtraction(string firstNumber, string secondNumber)
+        {
+            if (_utils.IsNumeric(firstNumber) && _utils.IsNumeric(secondNumber))
+            {
+                var sum = _utils.ConvertToDecimal(firstNumber) - _utils.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/5
+        [HttpGet("division/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Division(string firstNumber, string secondNumber)
+        {
+            if (_utils.IsNumeric(firstNumber) && _utils.IsNumeric(secondNumber))
+            {
+                var sum = _utils.ConvertToDecimal(firstNumber) / _utils.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/5
+        [HttpGet("multiplication/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Multiplication(string firstNumber, string secondNumber)
+        {
+            if (_utils.IsNumeric(firstNumber) && _utils.IsNumeric(secondNumber))
+            {
+                var sum = _utils.ConvertToDecimal(firstNumber) * _utils.ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        // GET api/values/5
+        [HttpGet("mean/{firstNumber}/{secondNumber}")]
+        public ActionResult<string> Mean(string firstNumber, string secondNumber)
+        {
+            if (_utils.IsNumeric(firstNumber) && _utils.IsNumeric(secondNumber))
+            {
+                var sum = (_utils.ConvertToDecimal(firstNumber) * _utils.ConvertToDecimal(secondNumber)) / 2;
+                return Ok(sum.ToString());
+
+            }
+            return BadRequest("Invalid Input");
+        }
+    }
+}
---
+++ RestCalculator/RestWithDotNetCore/Utils/Utils.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithDotNetCore.Utils
+{
+    public class Utils
+    {
+        public decimal ConvertToDecimal(string number)
+        {
+            decimal decimalValue;
+            if (decimal.TryParse(number, out decimalValue))
+            {
+                return decimalValue;
+            }
+            return 0;
+        }
+
+        public bool IsNumeric(string strNumber)
+        {
+            double number;
+            bool isNumber = double.TryParse(strNumber, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out number);
+            return isNumber;
+        }
+    }
+}
